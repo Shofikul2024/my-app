debbb6177faa6abb61a80f52f90249b0380494b4
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-24 02:01:21 +0600

    added  2 nd class

---
+++ temporary-app/class-2.py
@@ -0,0 +1,13 @@
+# integer type
+# x=15
+# y=20.555
+# print(x+y)
+
+
+
+singleQString='Upskill for a Better Career'
+print(singleQString)
+doubleQString="Upskill for a Better Career"
+print(doubleQString)
+tripleSQString='''Upskill for a Better Career'''
+print(tripleSQString)
