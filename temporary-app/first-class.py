949e38871583d8b1336db2abc437b1fbac573c74
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-23 20:07:48 +0600

    added  first class

---
+++ temporary-app/first-class.py
@@ -0,0 +1,20 @@
+def bangladesh():
+    print("Dhaka")
+    print("Rajshai")
+    print("Rangpur")
+    print("Khulna")
+    print("Barisal")
+    print("Cox")
+    print("Bandarbon")
+
+a=10
+b=20
+c=30
+print(c)
+bangladesh()
+d=40
+e=50
+print(e)
+f=60
+sum=a+b+c+d+e+f
+print(sum)
