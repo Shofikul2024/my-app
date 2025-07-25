084cd53f25955dde30093360c5c2b00f452b7039
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-25 15:27:53 +0600

    added  area

---
+++ python-pdf-ic/area.py
@@ -0,0 +1,15 @@
+# length=5
+# width=3
+# area=length*width
+# print('area', area)
+from operator import length_hint
+
+# area
+
+
+
+#
+# length=10
+# width=5
+# area=length*width
+# print('area', area)
