6d27d6f8971730b0105aade7e959407ca7de25cd
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-25 13:51:17 +0600

    added  input method

--- temporary-app/main.py
+++ temporary-app/main.py
@@ -8,8 +8,19 @@
 
 
 
+#
+# a= 50
+# b=20
+# sum=a+b
+# print(sum)
 
-a= 50
-b=20
-sum=a+b
-print(sum)
+
+
+#
+# # tuple
+# odd_numbers=(1,3,5,7,9,11)
+# print( type(odd_numbers))
+#
+# prime_number={ 2,43,5,7,11,}
+
+
